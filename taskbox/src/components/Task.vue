2a1c1829d9b6789f7ec7d3394a60a886e22bdb13
yinm <yinm.cminor9@gmail.com>
2019-06-23 10:04:07 +0900

    Add Task component and stories

---
+++ taskbox/src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="list-item">
+    <input type="text" :readonly="true" :value="this.task.title" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'task',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
