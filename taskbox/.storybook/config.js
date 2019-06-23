3338c545c0efc0641ae5c3c5a6dfbe77e29802c2
yinm <yinm.cminor9@gmail.com>
2019-06-23 10:08:53 +0900

    Update config for Storybook

--- taskbox/.storybook/config.js
+++ taskbox/.storybook/config.js
@@ -1,7 +1,9 @@
 import { configure } from '@storybook/vue';
 
+import '../src/index.css'
+
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../src', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
