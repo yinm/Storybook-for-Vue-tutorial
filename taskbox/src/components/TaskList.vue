4bf902dc8a3cf4e538b87a1b03fd513c0e8abbac
yinm <yinm.cminor9@gmail.com>
2019-06-28 09:45:09 +0900

    Separate between Container component and Presentational component

--- taskbox/src/components/TaskList.vue
+++ taskbox/src/components/TaskList.vue
@@ -1,65 +1,20 @@
 <template>
   <div>
-    <div class="list-items" v-if="loading">
-      <div class="loading-item" v-for="(n, index) in 6" :key="index">
-        <span class="glow-checkbox" />
-        <span class="glow-text">
-          <span>Loading</span> <span>cool</span> <span>state</span>
-        </span>
-      </div>
-    </div>
-    <div class="list-items" v-if="noTasks && !this.loading">
-      <div class="wrapper-message">
-        <span class="icon-check" />
-        <div class="title-message">You have no tasks</div>
-        <div class="subtitle-message">Sit back and relax</div>
-      </div>
-    </div>
-    <div class="list-items" v-if="showTasks">
-      <task
-        v-for="(task, index) in tasksInOrder"
-        :key="index"
-        :task="task"
-        @archiveTask="$emit('archiveTask', $event)"
-        @pinTask="$emit('pinTask', $event)"
-      />
-    </div>
+    <pure-task-list :tasks="tasks" />
   </div>
 </template>
 
 <script>
-import Task from './Task'
+import PureTaskList from './PureTaskList'
+import { mapState } from 'vuex'
 
 export default {
   name: 'task-list',
-  props: {
-    loading: {
-      type: Boolean,
-      default: false
-    },
-    tasks: {
-      type: Array,
-      default() {
-        return []
-      }
-    }
-  },
   components: {
-    Task
+    PureTaskList
   },
   computed: {
-    noTasks() {
-      return this.tasks.length === 0
-    },
-    showTasks() {
-      return !this.loading && !this.noTasks
-    },
-    tasksInOrder() {
-      return [
-        ...this.tasks.filter(t => t.state === 'TASK_PINNED'),
-        ...this.tasks.filter(t => t.state !== 'TASK_PINNED')
-      ]
-    }
+    ...mapState(['tasks'])
   }
 }
 </script>
