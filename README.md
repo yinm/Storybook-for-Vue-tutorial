ee7b20c8ea752f13dc6a5279fbb214a0d3f6d81f
yinm <yinm.cminor9@gmail.com>
2019-06-29 17:05:07 +0900

    Use store in Storybook

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ ref: [Get started | Storybook Tutorial](https://www.learnstorybook.com/vue/en/ge
 ### [Assemble a composite component | Storybook Tutorial](https://www.learnstorybook.com/vue/en/composite-component/)
 - `addDecorator()` allows us to add some “context” to the rendering of each task
   - Decorators are a way to provide arbitrary wrappers to stories
+  
+### [Construct a screen | Storybook Tutorial](https://www.learnstorybook.com/vue/en/screen/)
+- StorybookやUnit testsでは、最上位のコンポーネント以外ではStoreへアクセスしないようにする
+  - 最上位でアクセスしたデータを渡すことでレンダリングする
+
+
+
+
+
+
+
+
--- taskbox/src/components/PureInboxScreen.stories.js
+++ taskbox/src/components/PureInboxScreen.stories.js
@@ -1,11 +1,32 @@
+import { action } from '@storybook/addon-actions'
 import { storiesOf } from '@storybook/vue'
-import PureInboxScreen from './PureInboxScreen'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import { defaultTaskList } from './PureTaskList.stories'
+import PureInboxScreen from './PureInboxScreen.vue'
+
+Vue.use(Vuex)
+export const store = new Vuex.Store({
+  state: {
+    tasks: defaultTaskList
+  },
+  actions: {
+    pinTask(context, id) {
+      action('pinTask')(id)
+    },
+    archiveTask(context, id) {
+      action('archiveTask')(id)
+    }
+  }
+})
 
 storiesOf('PureInboxScreen', module)
   .add('default', () => {
     return {
       components: { PureInboxScreen },
       template: '<pure-inbox-screen />',
+      store,
     }
   })
   .add('error', () => {
